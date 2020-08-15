5f7c98874c7b5342689d8814583c0d4334ff357e
motovmp1 <vinicius.mpinho@gmail.com>
2020-08-15 19:49:41 +0100

    running test 2

--- src/com/edureka/HelloWorld.java
+++ src/com/edureka/HelloWorld.java
@@ -1,8 +1,19 @@
 package com.edureka;
 
 public class HelloWorld {
+    // here i cannot call, need crete a object class
+    public void nonStaticTest(){
+        System.out.println("Non static method print test");
+    }
+
+    public static void StaticVoid(){
+        System.out.println("Here java static will be call");
+    }
 
     public static void main (String[] args){
+        HelloWorld helloNonStaticObject = new HelloWorld();
         System.out.println("Hello World");
+        helloNonStaticObject.nonStaticTest();
+        StaticVoid();
     }
 }
