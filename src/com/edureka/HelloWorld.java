938b19684762ac8442e246e2d25c46909550a63c
motovmp1 <vinicius.mpinho@gmail.com>
2020-08-15 20:28:47 +0100

    running test 3

--- src/com/edureka/HelloWorld.java
+++ src/com/edureka/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public void nonStaticTest(){
         System.out.println("Non static method print test");
     }
-
+    // here i can call inside main without object created
     public static void StaticVoid(){
         System.out.println("Here java static will be call");
     }
@@ -13,7 +13,9 @@ public class HelloWorld {
     public static void main (String[] args){
         HelloWorld helloNonStaticObject = new HelloWorld();
         System.out.println("Hello World");
+        //here I call the object
         helloNonStaticObject.nonStaticTest();
+        // here I without call any object
         StaticVoid();
     }
 }
