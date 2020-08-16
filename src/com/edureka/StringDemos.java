5e6110a6f17ce4634bc8374bfd9ff6356c07304e
motovmp1 <vinicius.mpinho@gmail.com>
2020-08-16 10:24:59 +0100

    running test 5

---
+++ src/com/edureka/StringDemos.java
@@ -0,0 +1,14 @@
+package com.edureka;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
+public class StringDemos {
+    public static void main(String[] args) {
+        String s = "Edureka";
+        System.out.println(s.length());
+        System.out.println(s.substring(0));
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+
+    }
+}
